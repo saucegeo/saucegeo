4ae5eff459d3a5ac7ea713e1f5937eb35fa0cf24
Yacine Eldjidel <yacineeldjidel@gmail.com>
2026-01-22 23:58:15 -0500

    Changed the color palette of the README using 'rich' documentation

--- stats_cards.py
+++ stats_cards.py
@@ -6,6 +6,7 @@ from rich.text import Text
 from rich.align import Align
 import requests
 
+# color taken from https://rich.readthedocs.io/en/stable/appendix/colors.html
 
 def github_stats(user, repo, token=None):
     headers = {"Authorization": f"token {token}"} if token else {}
@@ -45,25 +46,25 @@ def generate_terminal_svg(stats):
 
     console.print("\n")
 
-    console.print("[bold yellow]saucegeo@github-profile ~ $ [/bold yellow]", end="")
-    console.print(" echo [red]'Hello there, my name is'[/red]\n")
-    console.print("[bold cyan]Hello there, my name is[/bold cyan]\n")
+    console.print("[khaki1]saucegeo@github-profile ~ $ [/khaki1]", end="")
+    console.print(" echo [indian_red1]'Hello there, my name is'[/indian_red1]\n")
+    console.print("[pale_turquoise1]Hello there, my name is[/pale_turquoise1]\n")
 
-    console.print("[bold yellow]saucegeo@github-profile ~ $ [/bold yellow]", end="")
+    console.print("[khaki1]saucegeo@github-profile ~ $ [/khaki1]", end="")
     console.print(" ascii-name\n")
 
-    console.print("[bold magenta]       ██╗   ██╗ █████╗  ██████╗██╗███╗   ██╗███████╗[/bold magenta]".center(70))
-    console.print("[bold magenta]       ╚██╗ ██╔╝██╔══██╗██╔════╝██║████╗  ██║██╔════╝[/bold magenta]".center(70))
-    console.print("[bold magenta]        ╚████╔╝ ███████║██║     ██║██╔██╗ ██║█████╗  [/bold magenta]".center(70))
-    console.print("[bold magenta]         ╚██╔╝  ██╔══██║██║     ██║██║╚██╗██║██╔══╝  [/bold magenta]".center(70))
-    console.print("[bold magenta]          ██║   ██║  ██║╚██████╗██║██║ ╚████║███████╗[/bold magenta]".center(70))
-    console.print("[bold magenta]          ╚═╝   ╚═╝  ╚═╝ ╚═════╝╚═╝╚═╝  ╚═╝╚══════╝[/bold magenta]\n".center(70))
-    console.print("[bold yellow]saucegeo@github-profile ~ $ [/bold yellow]", end="")
+    console.print("[indian_red1]      ██╗   ██╗ █████╗  ██████╗██╗███╗   ██╗███████╗[/indian_red1]".center(70))
+    console.print("[indian_red1]      ╚██╗ ██╔╝██╔══██╗██╔════╝██║████╗  ██║██╔════╝[/indian_red1]".center(70))
+    console.print("[indian_red1]       ╚████╔╝ ███████║██║     ██║██╔██╗ ██║█████╗  [/indian_red1]".center(70))
+    console.print("[indian_red1]        ╚██╔╝  ██╔══██║██║     ██║██║╚██╗██║██╔══╝  [/indian_red1]".center(70))
+    console.print("[indian_red1]         ██║   ██║  ██║╚██████╗██║██║ ╚████║███████╗[/indian_red1]".center(70))
+    console.print("[indian_red1]         ╚═╝   ╚═╝  ╚═╝ ╚═════╝╚═╝╚═╝  ╚═╝╚══════╝[/indian_red1]\n".center(70))
+    console.print("[khaki1]saucegeo@github-profile ~ $ [/khaki1]", end="")
     console.print(" github-stats")
 
-    table = Table(show_header=True, header_style="bold magenta", expand=False, box=None, padding=(0, 1))
-    table.add_column("", style="cyan", no_wrap=True)
-    table.add_column("", style="bright_magenta", justify="right")
+    table = Table(show_header=True, header_style="indian_red1", expand=False, box=None, padding=(0, 1))
+    table.add_column("", style="pale_turquoise1", no_wrap=True)
+    table.add_column("", style="indian_red1", justify="right")
 
     table.add_row("Stars", f"{stats['Stars']}")
     table.add_row("Total Commits", f"{stats['Total Commits']}")
@@ -73,33 +74,33 @@ def generate_terminal_svg(stats):
     console.print(table)
     console.print("")
 
-    console.print("[bold yellow]saucegeo@github-profile ~ $ [/bold yellow]", end="")
+    console.print("[khaki1]saucegeo@github-profile ~ $ [/khaki1]", end="")
     console.print(" about-me\n")
-    console.print(Align.center("[bright_magenta]I'm a computer engineering student with a passion \n    for Robotics, Hardware, and Open Source.[/bright_magenta]\n", vertical="middle"))
+    console.print(Align.center("[light_pink1]I'm a computer engineering student with a passion \n    for Robotics, Hardware, and Open Source.[/light_pink1]\n", vertical="middle"))
 
-    console.print("[bold yellow]saucegeo@github-profile ~ $ [/bold yellow]", end="")
+    console.print("[khaki1]saucegeo@github-profile ~ $ [/khaki1]", end="")
     console.print(" learning-roadmap\n")
     console.print("╔═════════════════════════════════════════╗")
     console.print("║            Currently Learning           ║")
     console.print("╠═════════════════════════════════════════╣")
     console.print("║                                         ║")
-    console.print("║  [cyan]Embedded C & Robotics:[/cyan]                 ║")
-    console.print("║  [bright_magenta]*** ROS2                               [/bright_magenta]║")
-    console.print("║  [bright_magenta]*** Assembly Programming               [/bright_magenta]║")
+    console.print("║  [indian_red1]Embedded C & Robotics:[/indian_red1]                 ║")
+    console.print("║  [pale_turquoise1]*** ROS2                               [/pale_turquoise1]║")
+    console.print("║  [pale_turquoise1]*** Assembly Programming               [/pale_turquoise1]║")
     console.print("║                                         ║")
-    console.print("║  [cyan]Web/UI:[/cyan]                                ║")
-    console.print("║  [bright_magenta]*** Typescript                         [/bright_magenta]║")
-    console.print("║  [bright_magenta]*** React + Vite                       [/bright_magenta]║")
+    console.print("║  [indian_red1]Web/UI:[/indian_red1]                                ║")
+    console.print("║  [pale_turquoise1]*** Typescript                         [/pale_turquoise1]║")
+    console.print("║  [pale_turquoise1]*** React + Vite                       [/pale_turquoise1]║")
     console.print("║                                         ║")
-    console.print("║  [cyan]Tools:[/cyan]                                 ║")
-    console.print("║  [bright_magenta]*** Linux/Bash                         [/bright_magenta]║")
-    console.print("║  [bright_magenta]*** Git & GitHub                       [/bright_magenta]║")
+    console.print("║  [indian_red1]Tools:[/indian_red1g]                                 ║")
+    console.print("║  [pale_turquoise1]*** Linux/Bash                         [/pale_turquoise1]║")
+    console.print("║  [pale_turquoise1]*** Git & GitHub                       [/pale_turquoise1]║")
     console.print("║                                         ║")
     console.print("╚═════════════════════════════════════════╝\n")
 
-    console.print("[bold yellow]saucegeo@github-profile ~ $ [/bold yellow]", end="")
+    console.print("[khaki1]saucegeo@github-profile ~ $ [/khaki1]", end="")
     console.print(" exit\n")
-    console.print("[bright_magenta]thank you for passing by ( ˘▽˘)っ♨ [/bright_magenta]\n")
+    console.print("[light_pink1]thank you for passing by ( ˘▽˘)っ♨ [/light_pink1]\n")
 
     svg = console.export_svg()
     svg = svg.replace('<svg ', '<svg style="width:100%; max-width:600px; height: auto;" ')
